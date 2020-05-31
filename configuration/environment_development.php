716651d1d4d2d28b4a54bb744974b811daeb0927
Marien Fressinaud <dev@marienfressinaud.fr>
2020-05-31 13:06:07 +0200

    tec: Configure SMTP server

--- configuration/environment_development.php
+++ configuration/environment_development.php
@@ -19,4 +19,19 @@ return [
         'username' => $dotenv->pop('DB_USERNAME'),
         'password' => $dotenv->pop('DB_PASSWORD'),
     ],
+
+    'mailer' => [
+        'type' => $dotenv->pop('APP_MAILER'),
+        'from' => $dotenv->pop('SMTP_FROM'),
+        'smtp' => [
+            'domain' => $dotenv->pop('SMTP_DOMAIN'),
+            'host' => $dotenv->pop('SMTP_HOST'),
+            'port' => intval($dotenv->pop('SMTP_PORT')),
+            'auth' => (bool)$dotenv->pop('SMTP_AUTH'),
+            'auth_type' => $dotenv->pop('SMTP_AUTH_TYPE'),
+            'username' => $dotenv->pop('SMTP_USERNAME'),
+            'password' => $dotenv->pop('SMTP_PASSWORD'),
+            'secure' => $dotenv->pop('SMTP_SECURE'),
+        ],
+    ],
 ];
