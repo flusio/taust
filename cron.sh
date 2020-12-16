78e95e6727a64933825e0d805d0d6b5dd3078ea4
Marien Fressinaud <dev@marienfressinaud.fr>
2020-12-16 19:49:47 +0100

    tec: Add clean command to cron.sh (run 1 out of 60)

--- cron.sh
+++ cron.sh
@@ -3,3 +3,5 @@ CURRENT_DIR=`dirname "$0"`
 php $CURRENT_DIR/cli --request /domains/heartbeats > /dev/null
 php $CURRENT_DIR/cli --request /alarms/monitor > /dev/null
 php $CURRENT_DIR/cli --request /alarms/notify > /dev/null
+
+[ $(( $RANDOM % 60 )) == 0 ] && php $CURRENT_DIR/cli --request /system/clear-old > /dev/null
